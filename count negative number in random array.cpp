eda53d33d91bef3d76210b49fb860c9aa98555a6
Ziad Mahmoud saad <zm5830537@gmail.com>
2026-05-19 11:23:41 +0300

    Add files via upload

---
+++ count negative number in random array.cpp
@@ -0,0 +1,56 @@
+// count odd numbers in random array.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int takearrayelementsnumber()
+{
+	int number;
+	cout << "please enter array elements number" << endl;
+	cin >> number;
+	return number;
+
+}
+int randomnumbers(int from, int to)
+{
+	int x = rand() % (to - from + 1) + from;
+	return x;
+}
+void fillAndPrintRandomArray(int arr1[100], int& num)
+{
+	cout << endl << endl;
+	cout << "random array is " << endl;
+	for (int a = 0; a < num; a++)
+	{
+		arr1[a] = randomnumbers(-100, 100);
+		cout << arr1[a] << " ";
+	}
+}
+int negativenumberscount(int arr1[100], int num)
+{
+	int count = 0;
+	for (int b = 0; b < num; b++)
+	{
+		if (arr1[b] < 0)
+		{
+			count++;
+		}
+	}
+	cout << endl << endl;
+	return count;
+}
+void printnegativenumberscount(int x)
+{
+	cout << "negative number count is " << x << endl;
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	int r = takearrayelementsnumber();
+	int j[100];
+	fillAndPrintRandomArray(j, r);
+	printnegativenumberscount(negativenumberscount(j, r));
+
+}
